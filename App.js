f0208b53ad48d75b837968beeb9f38a893d2da7e
CarlBondin <82595730+CarlBondin@users.noreply.github.com>
2022-04-09 10:06:56 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
     return (
       <PatientProvider>
         <HeightWeightProvider>
-          <Weight/>
+          <Height/>
         </HeightWeightProvider>
       </PatientProvider>
     )
