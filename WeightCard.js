f27915872d5a14807a6f271a816458650c8fb7b9
CarlBondin <82595730+CarlBondin@users.noreply.github.com>
2022-04-09 10:45:55 +0200

    Add files via upload

--- WeightCard.js
+++ WeightCard.js
@@ -1,15 +1,10 @@
 import React from "react";
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from '@mui/material/FormControl';
+import Input from "./Input";
 import './WeightCard.css';
 
 const WeightCard = ({handleChange, weight}) => (
     <div className="my-weight-card mdc-card mdc-card--outlined">
-        <FormControl sx={{['@media (min-width:720px)']: {minWidth: 280}, ['@media (max-width:720px)']: {minWidth: 250} }}>
-            <InputLabel id="blabel" sx={{fontSize: 17}}>Enter Weight</InputLabel>
-            <OutlinedInput name="weight" label="Enter Weight" handleChange={handleChange} value={weight} required/>
-        </FormControl> 
+        <Input name="weight" label="Enter weight" handleChange={handleChange} value={weight} units="KG"/>
     </div>
 )
 
