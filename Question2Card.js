0527f079b34b828feb7f72bdca2f32a10191a3ba
CarlBondin <82595730+CarlBondin@users.noreply.github.com>
2022-04-10 20:38:17 +0200

    Add files via upload

--- Question2Card.js
+++ Question2Card.js
@@ -4,9 +4,9 @@ import './QuestionCard.css';
 
 const question = "Was the weight lost due to exercise?";
 
-const QuestionCard = ({handleButtonSubmit, weightloss, exercise}) => (
+const QuestionCard = ({handleButtonSubmit, exercise}) => (
     <div className="my-question-card mdc-card mdc-card--outlined">
-       <Question handleButtonSubmit={handleButtonSubmit} title={question} value={weightloss} name={"wtloss"}/>
+       <Question handleButtonSubmit={handleButtonSubmit} title={question} value={exercise} name={"wtloss"}/>
     </div>
 )
 
--- QuestionCard.js
+++ QuestionCard.js
@@ -4,7 +4,7 @@ import './QuestionCard.css';
 
 const question = "Has the patient lost any weight in the past year?";
 
-const QuestionCard = ({handleButtonSubmit, weightloss, exercise}) => (
+const QuestionCard = ({handleButtonSubmit, weightloss}) => (
     <div className="my-question-card mdc-card mdc-card--outlined">
        <Question handleButtonSubmit={handleButtonSubmit} title={question} value={weightloss} name={"wtloss"}/>
     </div>
