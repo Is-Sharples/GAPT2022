c26d1b89f72ac80257ed0218c0c4ca6dc4c5c5ba
Shaun Attard <shaun.attard.20@um.edu.mt>
2022-05-02 11:53:32 +0200

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -9,12 +9,12 @@ export default function menu() {
         <Header typography = {typography} history = {"/"} name={"Menu"} /> 
 
         <Grid display='flex' justify={'center'} align='center' container rowGap={5} flexDirection={'column'} style={{width:"100%"}}/*columns={{xs:2, sm: 2, md:2}}*/ >
-            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginTop: '50px', backgroundColor:'#01497A', borderRadius: '20px'}}>Nurse</Button>
-            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', backgroundColor:'#01497A', borderRadius: '20px'}}>Physiotherapist</Button>
-            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', backgroundColor:'#01497A', borderRadius: '20px'}}>Doctor</Button>
-            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', backgroundColor:'#01497A', borderRadius: '20px'}}>Social Worker</Button>
-            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', backgroundColor:'#01497A', borderRadius: '20px'}}>Other Profession</Button>
-            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', backgroundColor:'#01497A', borderRadius: '20px'}}>Geriatric Consultant</Button>
+            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginLeft: '41.5%', marginTop: '50px', backgroundColor:'#01497A', borderRadius: '20px'}}>Nurse</Button>
+            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginLeft: '41.5%', backgroundColor:'#01497A', borderRadius: '20px'}}>Physiotherapist</Button>
+            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginLeft: '41.5%', backgroundColor:'#01497A', borderRadius: '20px'}}>Occupational Therapist</Button>
+            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginLeft: '41.5%', backgroundColor:'#01497A', borderRadius: '20px'}}>Social Worker</Button>
+            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginLeft: '41.5%', backgroundColor:'#01497A', borderRadius: '20px'}}>Other Profession</Button>
+            <Button variant='contained' style ={{maxWidth: '400px', minHeight: '80px', fontSize:'28px', justifyContent:'center', marginLeft: '41.5%', backgroundColor:'#01497A', borderRadius: '20px'}}>Geriatric Consultant</Button>
         </Grid>
 
     </div>
