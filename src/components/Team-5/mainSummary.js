94a412a4fd7f739f36c335a00864322a5e0f7a1a
Kryanu <andrew.sharples.20@um.edu.mt>
2022-08-14 21:13:53 +0200

    transformed mainsummary to functional component

--- src/components/Team-5/mainSummary.js
+++ src/components/Team-5/mainSummary.js
@@ -4,27 +4,26 @@ import DataTable from "./dataTable";
 import { RowData, BarthelRowData } from "./constants";
 import { Tabs } from "@mui/material";
 import { Tab } from "@material-ui/core";
+import { useState, useEffect } from "react";
 
 import "../styles/Summary.css";
 
-class MainSummary extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            tabValues:"Barthel",
-            rows: RowData,
-        };
-    }
-    tabChange = (e, newEvent) => {
-        this.setState( {tabValues:newEvent});
+export default function(props){
+    
+        
+    const [tabValues, setTabValues] = useState("Barthel");
+        
+    
+    const tabChange = (e, newEvent) => {
+        setTabValues(newEvent);
         
     }
-    render(){
+    
         let rows = []
-        if(this.state.tabValues === "Barthel"){
+        if(tabValues === "Barthel"){
             rows = RowData 
         }else{
-            rows =BarthelRowData
+            rows = BarthelRowData
         }
         return (
             <div className="flex flex-col items-center bg-neutral-200 min-h-screen">
@@ -33,20 +32,17 @@ class MainSummary extends React.Component{
                 <div className="flex flex-col font-semibold rounded-xl shadow-lg mx-auto w-3/4 h-auto p-3 max-w-2xl bg-white mb-5 mt-4 text-center">
                     <p className="name">Andrew Sharples</p>
                     <p className="id">0218702L</p>
-            
                 </div>  
-                <Tabs className="tabs" value={this.state.tabValues} onChange={this.tabChange}>
+                <Tabs className="tabs" value={tabValues} onChange={tabChange}>
                     <Tab className="tab" value = {"Barthel"} label="Barthel Index" ></Tab>
                     <Tab value = {"HeightWeight"} label = "Height & Weight" ></Tab>
                 </Tabs>
                 <div className='flex justify-center w-3/4'>
-                    <DataTable RowData={rows} ></DataTable>
+                    <DataTable title={tabValues} RowData={rows} ></DataTable>
                 </div>
                 
 
             </div>
         );
-    }
+    
 }
-
-export default MainSummary;
